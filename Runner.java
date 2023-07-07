2bcea1180aa0acfdbf443c929572b190fd5ada1f
Renato Stanz <stanz@tutamail.com>
2023-07-07 17:27:16 -0300

    C07 - Added the Runner

---
+++ Runner.java
@@ -0,0 +1,22 @@
+public class Runner{
+
+	public static void main(String[] args){
+
+		System.out.println("The program has started");
+		FileHandler dataSet 	= new FileHandler();
+		FileHandler patients 	= new FileHandler("")
+		System.out.println("Dataset and the patients data were" +
+			           " called with default file addresses"+
+				   "\n (documents/SintomasMenor.txt and"+
+				   "documents/pacientes.txt)");
+		System.out.println("Calling the tree builder");
+		dataSet.treeBuild();
+		System.out.println("Tree builder done!");
+		System.out.println("Calling the method to get the "+
+				   "patients prognostics.");
+		patients.getPrognostics();
+		System.out.println("All done, please check the file on "+
+				   "documents/pacientes.txt to check if"+
+				   " its ok.");
+	}
+}
